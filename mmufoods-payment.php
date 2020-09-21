5f957234f9f362535c6f82b3397a0a44e47451f2
nathan tugume <41728039+nathantugume@users.noreply.github.com>
2020-09-21 11:13:34 +0300

    Create mmufoods-payment.php
    make mobile money payments on mmufoods project

---
+++ mmufoods-payment.php
@@ -0,0 +1,16 @@
+<?php 
+/**
+ * Plugin Name: mmufoods-Payment
+ * Plugin Uri: www.github.com/nathantugume
+ * Author Name: Nathan Tugume
+ * Author URI: https://github.com/nathantugume
+ * Description: This plugin allows mobile money payments on wocommerce systems
+ * Version: 0.0.1
+ * Licence: 0.1
+ * License URL: http://www.gnu.org/licenses/gpl-2.0.txt
+ * text-domain: mmufoods payment gateway
+ */
+
+
+
+?>
